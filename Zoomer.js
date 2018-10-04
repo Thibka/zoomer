2f73e7144083f47640db09992f3df09561538937
Thibaut Foussard <thibaut.foussard@wonderbanners.com>
2018-10-04 12:51:02 +0200

    Version 1.3

--- Zoomer.js
+++ Zoomer.js
@@ -1,4 +1,4 @@
-// Zoomer v. 1.2
+// Zoomer v. 1.3
 (function(window, document, undefined){
     var getTime = Date.now || function() {return new Date().getTime();};
     function Zoom(params) {
@@ -8,6 +8,7 @@
         this.image         = new Image();
         this.CANVAS_BASE_X = this.CANVAS_BASE_Y = 0;
         this.easing        = params.easing || function easeInOutQuad(n,u,e,t){return(n/=t/2)<1?e/2*n*n+u:-e/2*(--n*(n-2)-1)+u};
+        this.currentStep   = 1;
 
         this.canvas.setAttribute('width', this.params.format.width);
         this.canvas.setAttribute('height', this.params.format.height);
@@ -25,10 +26,10 @@
             this.ctx.clearRect(0, 0, this.params.format.width, this.params.format.height)
             this.ctx.drawImage(
                 this.image, 
-                this.params.start.x,
-                this.params.start.y,
-                this.params.start.width, 
-                this.params.start.height, 
+                this.params.steps[0].x,
+                this.params.steps[0].y,
+                this.params.steps[0].width, 
+                this.params.steps[0].height, 
                 this.CANVAS_BASE_X, this.CANVAS_BASE_Y, this.params.format.width, this.params.format.height
             );
 
@@ -46,21 +47,26 @@
 
             this.ctx.clearRect(0, 0, this.params.format.width, this.params.format.height);
 
-            var frameZoomLevelX = this.easing(this.clock, this.params.start.width, this.params.end.width - this.params.start.width, this.params.duration),
-                frameZoomLevelY = this.easing(this.clock, this.params.start.height, this.params.end.height - this.params.start.height, this.params.duration),
-                frameX = this.easing(this.clock, this.params.start.x, this.params.end.x - this.params.start.x, this.params.duration),
-                frameY = this.easing(this.clock, this.params.start.y, this.params.end.y - this.params.start.y, this.params.duration);
+            var frameZoomLevelX = this.easing(this.clock, this.params.steps[this.currentStep-1].width, this.params.steps[this.currentStep].width - this.params.steps[this.currentStep-1].width, this.params.steps[this.currentStep].duration),
+                frameZoomLevelY = this.easing(this.clock, this.params.steps[this.currentStep-1].height, this.params.steps[this.currentStep].height - this.params.steps[this.currentStep-1].height, this.params.steps[this.currentStep].duration),
+                frameX = this.easing(this.clock, this.params.steps[this.currentStep-1].x, this.params.steps[this.currentStep].x - this.params.steps[this.currentStep-1].x, this.params.steps[this.currentStep].duration),
+                frameY = this.easing(this.clock, this.params.steps[this.currentStep-1].y, this.params.steps[this.currentStep].y - this.params.steps[this.currentStep-1].y, this.params.steps[this.currentStep].duration);
 
             this.ctx.drawImage(
-                    this.image, 
-                    frameX, 
-                    frameY, 
-                    frameZoomLevelX, 
-                    frameZoomLevelY, 
-                    this.CANVAS_BASE_X, this.CANVAS_BASE_Y, this.params.format.width, this.params.format.height
-                );
+                this.image, 
+                frameX, 
+                frameY, 
+                frameZoomLevelX, 
+                frameZoomLevelY, 
+                this.CANVAS_BASE_X, this.CANVAS_BASE_Y, this.params.format.width, this.params.format.height
+            );
 
-            if (this.clock < this.params.duration) requestAnimationFrame(this.animate.bind(this));
+            if (this.clock < this.params.steps[this.currentStep].duration) requestAnimationFrame(this.animate.bind(this));
+            else if (this.currentStep < this.params.steps.length - 1) {
+                this.currentStep ++;
+                this.clock = 0;
+                this.start();
+            }
         }
 
     window.Zoom = Zoom;
